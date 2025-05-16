f5e2af187e6a83e343e2c025c3f9bf7611a1b572
KindaTired <mertcansahinileri@gmail.com>
2025-05-16 13:19:21 +0200

    Manifest 3

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var i = 0;
-function buttonclick(){
-    
-    while (true) {
-        
-        setInterval(function(){
-            document.getElementsByClassName("btn-control-md btn-full-width")[0].click();
-        }, 300);
-        i++
-        if (i = 20) {            
-            break;
-        } 
-    }
+// script.js (Fixed)
+let clicks = 0;
+const MAX_CLICKS = 40;
+
+function buttonclick() {
+    const intervalId = setInterval(() => {
+        const buttons = document.getElementsByClassName("btn-control-md btn-full-width");
+        if (buttons.length > 0 && clicks < MAX_CLICKS) {
+            buttons[0].click();
+            clicks++;
+        } else {
+            clicks = 0
+            clearInterval(intervalId); 
+        }
+    }, 650);
 }
